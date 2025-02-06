a4159d4c7af2fc9a828c0bf00eacbcf7415c8121
illyaro <panvalkon@gmail.com>
2025-02-06 18:49:07 +0100

    improved get query

--- rust/src/controller/records/get.rs
+++ rust/src/controller/records/get.rs
@@ -1,10 +1,41 @@
-use actix_web::{get, HttpResponse, http::header::ContentType};
-use crate::model::record::dao::get_all;
+// use actix_web::{get, HttpResponse, http::header::ContentType};
+// use crate::model::record::dao::get_all;
+
+// #[get("/records")]
+// pub async fn get() -> HttpResponse{
+//     let records = get_all();
+    
+//     HttpResponse::Ok()
+//         .insert_header(ContentType::json())
+//         .json(records)
+// }
+
+use crate::model::record::dao::{get_all, get_all_of_user_and_date_interval};
+use actix_web::{get, http::header::ContentType, web, HttpResponse};
+use chrono::NaiveDateTime;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct QueryParams {
+    user_id: Option<String>,
+    dt_from: Option<NaiveDateTime>,
+    dt_to: Option<NaiveDateTime>,
+}
 
 #[get("/records")]
-pub async fn get() -> HttpResponse{
-    let records = get_all();
-    
+pub async fn get(path: web::Query<QueryParams>) -> HttpResponse {
+    let query_params = path.into_inner();
+
+    let records = if let (Some(user_id), Some(dt_from)) = (query_params.user_id, query_params.dt_from) {
+        let dt_to = query_params
+            .dt_to
+            .unwrap_or_else(|| chrono::Local::now().naive_local());
+        
+        get_all_of_user_and_date_interval(user_id, dt_from, dt_to)
+    } else {
+        get_all()
+    };
+
     HttpResponse::Ok()
         .insert_header(ContentType::json())
         .json(records)
